b3fd6506fea2cc10a50eefcffad4bd1ae03d247d
Wahyu Restu Pamuji <68573221+WahyuOzora1@users.noreply.github.com>
2022-02-26 10:16:25 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -100,4 +100,14 @@ $(document).ready(function () {
 
 
 
+$(".animated-progress span").each(function () {
+    $(this).animate(
+      {
+        width: $(this).attr("data-progress") + "%",
+      },
+      1000
+    );
+    $(this).text($(this).attr("data-progress") + "%");
+  });
+
 
